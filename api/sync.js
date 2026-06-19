b053577ce53570dbcc2acf2aa1dab42d48134153
Rodrigo Ramos <40925093+rodrigobxr@users.noreply.github.com>
2026-06-18 21:34:06 -0300

    sync.js

--- api/sync.js
+++ api/sync.js
@@ -1,1 +1,13 @@
-sync.js
+export default async function handler(req, res) {
+  try {
+    const dadosAtualizados = {
+      sucesso: true,
+      data_hora: new Date().toISOString(),
+      mensagem: "Sincronização via servidor concluída com sucesso!"
+    };
+    res.status(200).json(dadosAtualizados);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ sucesso: false, erro: 'Falha ao buscar dados na internet' });
+  }
+}
